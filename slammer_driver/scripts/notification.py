e2eb50f38896335fb7c762ffcb2da1f6afab321b
Alessio Morale <alessiomorale@gmail.com>
2020-08-08 22:57:08 +0100

    [DRIVER] - fix launch files, port and update notification leds indexes

--- slammer_driver/scripts/notification.py
+++ slammer_driver/scripts/notification.py
@@ -13,14 +13,25 @@ battery_current = 0
 battery_charge = 0
 battery_percentage = 0
 
+battery_warn_threshold = 10.9
+battery_critical_threshold = 10.6
 
-front = 3
-lamp = 0
+
+front = 0
+lamp = 3
 rear = 1
 front_count = 8
 rear_count = 20
 
+# R, G, B
+front_colour = [ 64, 64, 96]
+rear_colour  = [16, 0, 0]
 
+lamp_max = 32
+
+lamp_batt_ok = [lamp_max, lamp_max, lamp_max]
+lamp_batt_warn = [lamp_max // 2, lamp_max, 0]
+lamp_batt_critical = [0, lamp_max, 0]
 
 def on_batterystate(msg):
     global battery_voltage
@@ -45,28 +56,33 @@ if __name__ == '__main__':
     led.msecOff = 0
     led.single = False
 
+    rospy.rostime.wallsleep(5)
+
+
     init = True
     # start running
     while not rospy.core.is_shutdown():
         if init:
-                #front white
+
+            #front white
             led.group = front
-            led.R = 64
-            led.G = 64
-            led.B = 0
+            led.R = front_colour[0]
+            led.G = front_colour[1]
+            led.B = front_colour[2]
             for i in range(front_count):
                 led.led = i
-                #pub.publish(led)
+                pub.publish(led)
                 rospy.rostime.wallsleep(0.1)
+
             #Read red
-            led.R = 0
-            led.G = 0
-            led.B = 64
+            led.R = rear_colour[0]
+            led.G = rear_colour[1]
+            led.B = rear_colour[2]
             led.group = rear
             for i in range(rear_count):
                 led.led = i
-                #pub.publish(led)
-                rospy.rostime.wallsleep(0.1)
+                pub.publish(led)
+                rospy.rostime.wallsleep(0.01)
 
             init = False
 
@@ -74,17 +90,18 @@ if __name__ == '__main__':
         led.led = 0
         led.msecOn = 50
         led.msecOff = 0
-        if(battery_voltage > 10.8):
-            led.R = 64
-            led.G = 0
-            led.B = 0
-        elif battery_voltage >10.5:
-            led.R = 32
-            led.G = 64
-            led.B = 0
+        led.single = True
+        if(battery_voltage > battery_warn_threshold):
+            led.R = lamp_batt_ok[0]
+            led.G = lamp_batt_ok[1]
+            led.B = lamp_batt_ok[2]
+        elif battery_voltage > battery_critical_threshold:
+            led.R = lamp_batt_warn[0]
+            led.G = lamp_batt_warn[1]
+            led.B = lamp_batt_warn[2]
         else:
-            led.R = 0
-            led.G = 64
-            led.B = 0
+            led.R = lamp_batt_critical[0]
+            led.G = lamp_batt_critical[1]
+            led.B = lamp_batt_critical[2]
         pub.publish(led) 
-        rospy.rostime.wallsleep(1.0)
+        rospy.rostime.wallsleep(2.0)
